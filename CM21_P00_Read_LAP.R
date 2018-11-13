ea3843b7b979ee7beaa1a7bb1a4bdb51c2da8253
Natsis Thanasis on crane <natsisthanasis@gmail.com>
2018-11-14 00:07:23 +0200

    auto update 2018-11-14-00:07:23

--- CM21_P00_Read_LAP.R
+++ CM21_P00_Read_LAP.R
@@ -8,12 +8,40 @@
 #'
 
 
+####  Set environment  ####
 closeAllConnections()
 rm(list = (ls()[ls() != ""]))
 Sys.setenv(TZ = "UTC")
 tic = Sys.time()
 Script.Name = c("CM_P00")
 
-
+####  . . Variables  ####
 source("/home/athan/CM_21_GLB/DEFINITIONS.R")
+
+
+
+
+
+####  Data input  ####
+
+sirena_files <- list.files( path        = SIRENA_DIR,
+                            recursive   = TRUE,
+                            pattern     = "[0-9]*06.LAP",
+                            ignore.case = TRUE,
+                            full.names  = TRUE )
+cat(paste(length(sirena_files), "files from Sirena\n"))
+
+
+radmon_files <- list.files( path        = RADMON_DIR,
+                            recursive   = TRUE,
+                            pattern     = "[0-9]*06.LAP",
+                            ignore.case = TRUE,
+                            full.names  = TRUE )
+cat(paste(length(radmon_files), "files from Radmon\n"))
+
+
+sir_names <- basename(sirena_files)
+rad_names <- basename(radmon_files)
+
+# sir_names[ ! sir_names %in% rad_names]
 
--- DEFINITIONS.R
+++ DEFINITIONS.R
@@ -9,10 +9,18 @@
 #'
 
 
-####  Data location  ####
+## Project root folder
+BASED     = "/home/athan/CM_21_GLB/"
 
+
+####  Data location  ####
+SUN_FOLDER = "/home/athan/DATA_RAW/SUN/PySolar_LAP/"
+SIRENA_DIR = "/home/athan/DATA_RAW/Bband/AC21_LAP.GLB/"
+RADMON_DIR = "/home/athan/DATA_RAW/Raddata/6"
 
 ####  Variables  ####
 
-
+## date range to process
+START_DAY = as.POSIXct("2007-01-01 00:00:00 UTC")
+END_DAY   = as.POSIXct("2018-01-01 00:00:00 UTC")
 
