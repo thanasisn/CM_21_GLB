aca729249eed31828dc6b8a14ee8b7f78160f76f
Natsis Thanasis on crane <natsisthanasis@gmail.com>
2018-11-13 20:09:06 +0200

    auto update 2018-11-13-20:09:06

---
+++ DEFINITIONS.R
@@ -0,0 +1,18 @@
+#'
+#' Here are some global or common variables for this project.
+#' This variables define:
+#'   - calculations parameters
+#'   - data processing
+#'   - data path locations
+#'
+#'
+#'
+
+
+####  Data location  ####
+
+
+####  Variables  ####
+
+
+
