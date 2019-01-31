12ac470cce022d8068a9096d6144e8d8dea968b2
Natsis Thanasis on sagan <natsisathan@gmail.com>
2019-01-31 12:15:04 +0200

    auto update 2019-01-31-12:15:04

---
+++ CM21_functions.R
@@ -0,0 +1,231 @@
+
+Sys.setenv(TZ = "UTC")
+require(xts)  ## index command
+
+## check date function
+is.POSIXct <- function(x) inherits(x, "POSIXct")
+
+## values from CM21_caldata_06.txt
+## define data
+calibration_data = matrix(
+       c( "1991-01-01", 11.98E-6, 0.5E-2,
+          "1995-10-21", 11.98E-6, 2E-2,
+          "1995-11-01", 11.98E-6, 1E-2,
+          "2004-07-01", 11.98E-6, 4E-2,
+          "2005-12-05", 11.99E-6, 4E-2,
+          "2011-12-30", 11.96E-6, 4E-2,
+          "2012-01-31", 11.96E-6, 4E-2  ),
+       byrow = TRUE,
+       ncol = 3)
+## read to data frame
+calibration_data = data.frame(Date = as.POSIXct( calibration_data[,1] ),
+                              Sens = as.numeric( calibration_data[,2] ),
+                              Gain = as.numeric( calibration_data[,3] ) )
+
+## interpolation functions
+sensitivity = approxfun( x = calibration_data$Date,
+                         y = calibration_data$Sens,
+                         rule = 1:2  )
+gain        = approxfun( x = calibration_data$Date,
+                         y = calibration_data$Gain,
+                         rule = 1:2  )
+
+
+#' Convertion factor from volt to watt for CM21
+#'
+#' @details This uses both the sensitivity of the instruments and the gain
+#' factor of the measurmetna device.
+#'
+#' @param date POSIXct date for the factor
+#'
+#' @return (numeric) Convertion factor
+#' @family CM 21 functions
+#' @export
+cm21factor = function(date) {
+    if (is.POSIXct(date)) {
+        return( gain(date) / sensitivity(date) )
+    } else {
+        stop("input must be POSIXct.\n you gave : ",date)
+    }
+}
+
+rm(calibration_data)
+
+
+
+## similar to RAersosols::get_dark_day
+## computes values to be used for dark correction
+dark_calculations <- function(elevatio, darklim, dates, dstretch, values) {
+    ## find noun
+    nounindex  = match( max(elevatio), elevatio )
+    ## split day
+    selectmorn = elevatio < darklim & index(elevatio) < nounindex
+    selecteven = elevatio < darklim & index(elevatio) > nounindex
+    ## all morning and evening dates
+    morning    = dates[selectmorn]
+    evening    = dates[selecteven]
+
+    ## morning selection with time limit
+    mornigend   = morning[max(index(morning))]
+    mornigstart = mornigend - dstretch
+    ## selection for morning dark
+    morningdark = selectmorn & dates <= mornigend & mornigstart < dates
+
+    ## evening selection with time limitedLabels
+    eveningstart  = evening[min(index(evening))]
+    eveningend    = eveningstart + dstretch
+    ## selection for evening dark
+    eveningdark = selecteven & dates >= eveningstart & dates < eveningend
+
+    ## Morning Dark
+    datavalues_M = values[morningdark]
+    datedates_M  = dates[ morningdark]
+    ## Evening Dark
+    datavalues_E = values[eveningdark]
+    datedates_E  = dates[ eveningdark]
+
+    return(list( Mavg = mean(   datavalues_M,  na.rm = TRUE ),
+                 Mmed = median( datavalues_M,  na.rm = TRUE ),
+                 Msta = max(    datedates_M,   na.rm = TRUE ),
+                 Mend = min(    datedates_M,   na.rm = TRUE ),
+                 Mcnt = sum(!is.na(datavalues_M)),
+                 Eavg = mean(   datavalues_E,  na.rm = TRUE ),
+                 Emed = median( datavalues_E,  na.rm = TRUE ),
+                 Esta = min(    datedates_E,   na.rm = TRUE ),
+                 Eend = max(    datedates_E,   na.rm = TRUE ),
+                 Ecnt = sum(!is.na(datavalues_E))
+    ))
+
+}
+
+
+
+## similar to RAersosols::linear_dark_function
+dark_correction <- function(dark_day, DCOUNTLIM, type, dd, test, missfiles, missingdark) {
+    ## ignore dark signal if too low counts
+    if (dark_day$Mcnt < DCOUNTLIM) { dark_day$Mmed = dark_day$Mavg = NA }
+    if (dark_day$Ecnt < DCOUNTLIM) { dark_day$Emed = dark_day$Eavg = NA }
+
+    if (type == "mean") {
+        # print("use mean for dark correction")
+        ## Values to use
+        dar_x = c(dark_day$Msta, dark_day$Esta)
+        dar_y = c(dark_day$Mavg, dark_day$Eavg)
+
+    } else if (type == "median") {
+        # print("use median for dark correction")
+        ## Values to use
+        dar_x = c(dark_day$Msta, dark_day$Esta)
+        dar_y = c(dark_day$Mmed, dark_day$Emed)
+
+    } else {
+        print("Dont know what to do")
+        stop("Invalid argument in Dark correction")
+    }
+
+
+    ## Choose scenario based on available dark information
+    ## with left and right dark
+    if (sum(is.na( dar_y )) == 0) {
+        dark_line = approxfun(x = dar_x,
+                              y = dar_y,
+                              rule = 2,
+                              method = "linear")
+    }
+
+    ## with only left dark
+    if ( sum(is.na( dar_y )) == 1 ) {
+        dark_line = approxfun(x = dar_x[!is.na( dar_y )],
+                              y = dar_y[!is.na( dar_y )],
+                              method = "constant",
+                              rule = 2 )
+    }
+
+    ## with out dark should break!
+    if ( sum(is.na( dar_y )) >= 2 ) {
+        text = paste( as.POSIXct(dd, origin = "1970-01-01" ), test, "No dark don't know what to do!! using ", missingdark, " for Dark!!" )
+        cat(text, sep = "\n")
+        warning(text)
+        cat(text, sep = "\n", file = missfiles, append = TRUE)
+
+        ## protect from empty variable
+        if (is.na(missingdark)) {
+            stop("no value for dark given")
+        }
+
+        ## create dummy dark signal for correction
+        ####FIXME this can change to a set value after prossecing from statistics -----
+        dark_line = approxfun(x = dar_x,
+                              y = c(missingdark, missingdark),
+                              rule = 2,
+                              method = "linear")
+
+        # stop("\n",message, "\nFunction:\nfdfsf\n")
+    }
+
+    return(dark_line)
+}
+
+
+
+
+plot_norm <- function(daydata, test, tag) {
+    ## Main data plot
+    dddd = min(daydata$Global, daydata$GLstd , na.rm = TRUE)
+    uuuu = max(daydata$Global, daydata$GLstd , na.rm = TRUE)
+    if (dddd > -5  ) { dddd = 0  }
+    if (uuuu < 190 ) { uuuu = 200}
+    ylim = c(dddd , uuuu)
+
+    plot(daydata$Date30, daydata$Global,
+         "l", xlab = "UTC", ylab = "W/m^2",
+         col  = "blue", lwd = 1.1, lty = 1, xaxt = "n", ylim = ylim )
+    abline(h = 0, col = "gray60")
+    abline(v   = axis.POSIXct(1, at = pretty(daydata$Date30, n = 12, min.n = 8 ), format = "%H:%M" ),
+           col = "lightgray", lty = "dotted", lwd = par("lwd"))
+    points(daydata$Date30, daydata$GLstd, pch = ".", cex = 2, col = "red" )
+    title( main = paste(test, format(daydata$Date30[1] , format = "  %F")))
+    text(daydata$Date30[1], uuuu, labels = tag, pos = 4, cex =.7 )
+}
+
+
+
+
+
+
+
+
+#' Get a nice string for seconds as formated time
+#'
+#' @param seconds Time duration in seconds
+#' @param type The output format. "h" "93:29:43.758", "d" "3 21:34:17.072", "s" "336936.990"
+#'
+#' @return
+#' @export
+#'
+countdownS <- function(seconds, type) {
+    sss    = as.numeric(seconds)
+    secs   = sss %% 60
+    mins   = sss %/% 60 %% 60
+    hours  = sss %/% 60 %/% 60
+    hoursd = hours  %% 24
+    days   = hours %/% 24
+
+    if (type == "h") {
+        ## multiple hours
+        output = sprintf("%02d:%02d:%06.3f", hours, mins, secs)
+    } else if ( type == "d" ) {
+        ## days hours
+        output = sprintf("%s %02d:%02d:%06.3f", days, hoursd, mins, secs)
+    } else if ( type == "s" ) {
+        output = sprintf("%06.3f", sss )
+    }
+    return(output)
+}
+
+
+
+
+
+
+
