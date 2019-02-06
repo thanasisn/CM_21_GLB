5d646a32bb336d3ea136232f2aa7b19699ed9050
Natsis Thanasis on kostas <natsisathan@gmail.com>
2019-02-06 16:12:19 +0200

    auto update 2019-02-06-16:12:19

---
+++ run_data_validation.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#set +e
+
+## Update data from sirena ##
+
+/home/athan/Aerosols/BASH_help/update_data_from_sirena.sh
+
+
+# ## Parse raw data and inspect ##
+# 
+# /home/athan/Aerosols/CM21datavalidation/CM_P01_Import_Data_v2.R
+# /home/athan/Aerosols/CM21datavalidation/CM_P01_Explore_Data.R
+# /home/athan/Aerosols/CM21datavalidation/CM_P01_suspects_plot_v2.R
+# 
+# 
+# ## Data filtering and correction. ##
+# 
+# /home/athan/Aerosols/CM21datavalidation/CM_P02_Import_Data_wo_bad_days_v3.R
+# /home/athan/Aerosols/CM21datavalidation/CM_P02_Explore_Data.R
+# /home/athan/Aerosols/CM21datavalidation/CM_P02_missing_dark.R
+# /home/athan/Aerosols/CM21datavalidation/CM_P02_suspects_plot_v2.R
+# 
+# 
+# ## Apply data filtering and correction ##
+# 
+# /home/athan/Aerosols/CM21datavalidation/CM_P03_Import_Data_wo_bad_days_v3.R
+# /home/athan/Aerosols/CM21datavalidation/CM_P03_suspects_plot_v2.R
+# # /home/athan/Aerosols/CM21datavalidation/CM_P03_Filter_Data.R
+# 
+# 
+# ## Export data ##
+# 
+# /home/athan/Aerosols/CM21datavalidation/CM_P04_Export_Data_v3.R
+# /home/athan/Aerosols/CM21datavalidation/CM_P04_Export_TOT.R
+# 
+# 
+# ## Check data ##
+# 
+# /home/athan/Aerosols/CM21datavalidation/CM_P05_Compare_TOT.R
+# /home/athan/Aerosols/CM21datavalidation/CM_P05_plot_lapori.R
+
+
+
+#set -e
