937120a91589814f0560069492112509b8053158
Natsis Thanasis on tyler <natsisthanasis@gmail.com>
2020-06-22 15:08:34 +0300

    auto update 2020-06-22-15:08:34

--- CM21_P60_GHI_Export_WRDC.R
+++ CM21_P60_GHI_Export_WRDC.R
@@ -87,7 +87,7 @@ tag = paste0("Natsis Athanasios LAP AUTH ", strftime(Sys.time(), format = "%b %Y
 
 
 #### .  . Export range  ####
-start_year  =  2008
+start_year  =  2006
 end_year    =  2020
 yearstodo   <- seq( start_year, end_year )
 
