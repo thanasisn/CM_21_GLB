964d7510bb7a62d92443ec7fed52c0de0c207f15
thanasisn <11601717+thanasisn@users.noreply.github.com>
2022-03-25 00:13:10 +0200

    Auto commit

--- check_export_sirena.R
+++ check_export_sirena.R
@@ -11,8 +11,10 @@ tic <- Sys.time()
 Script.Name <- tryCatch({ funr::sys.script() },
                         error = function(e) { cat(paste("\nUnresolved script name: ", e),"\n")
                             return("Undefined R script name!!") })
-if(!interactive())pdf(file="~/LOGs/car_logs/Car_stats_plots.pdf")
-if(!interactive())sink(file=sub("\\.R$",".out",Script.Name,),split=TRUE)
+if(!interactive()) {
+    pdf(file=sub("\\.R$",".pdf",Script.Name))
+    sink(file=sub("\\.R$",".out",Script.Name,),split=TRUE)
+}
 Script.Base = sub("\\.R$","",Script.Name)
 
 library(data.table)
@@ -23,12 +25,13 @@ rdsfile <- "~/DATA/cm21_data_validation/export_all_tot_dat.Rds"
 folder  <- "~/DATA/cm21_data_validation/AC21_lap.GLB_TOT"
 
 files <- list.files(folder,
-                    pattern     = "[0-9]*\\TOT.*.dat",
-                    recursive   = T,
-                    ignore.case = T,
-                    full.names  = T)
+                     pattern     = "[0-9]*\\TOT.*.dat",
+                     recursive   = T,
+                     ignore.case = T,
+                     full.names  = T)
+
 ## check if we have to read
-if (max(file.mtime(files)) > file.mtime(rdsfile)) {
+if (!file.exists(rdsfile) |  max(file.mtime(files)) > file.mtime(rdsfile)) {
     gather <- data.table()
     for (af in files) {
         temp      <- fread(af)
@@ -46,8 +49,6 @@ if (max(file.mtime(files)) > file.mtime(rdsfile)) {
 }
 
 
-gather[ gather$Date == "2019-03-07" ]
-
 
 
 ## FIXME this is bad!! why 24
@@ -55,14 +56,19 @@ table(gather$TIME_UT %/% 1)
 hist(round((gather$TIME_UT %% 1) * 60), breaks = 61)
 hist(gather$TIME_UT %/% 1, breaks = 25)
 
+gather[ TIME_UT %/% 1 == 24, unique(file)]
+
+gather[ TIME_UT %/% 1 == 24, .N, by = year(Date)]
+
+
 ## add nice date
 dateess     <- paste( gather$Date,  gather$TIME_UT %/% 1, round((gather$TIME_UT %% 1) * 60) )
 gather$Date <- as.POSIXct( strptime(dateess, "%F %H %M") )
-
+setorder(gather,Date)
 
 ## drop some data
 gather <- gather[ !is.na(`[W.m-2]`), ]
-gather <- gather[ `[W.m-2]` > 0    , ]
+gather <- gather[ `[W.m-2]` > -5   , ]
 
 
 ## test data
@@ -71,22 +77,22 @@ hist(gather$`[W.m-2]`)
 
 
 
-## apply exclusions this have been done
-ranges       <- read.table( "~/Aerosols/source_R/PARAMETRIC/skip_date_ranges_CM21.txt",
-                            sep = ";",
-                            colClasses = "character",
-                            header = TRUE, comment.char = "#" )
-ranges$From  <- strptime(ranges$From,  format = "%F %H:%M", tz = "UTC")
-ranges$Until <- strptime(ranges$Until, format = "%F %H:%M", tz = "UTC")
-
-for ( i in 1:nrow(ranges) ) {
-    lower <- ranges$From[  i ]
-    upper <- ranges$Until[ i ]
-    ## select to remove
-    select  <- gather$Date >= lower & gather$Date <= upper
-    gather  <- gather[ ! select ]
-    rm(select)
-}
+# ## apply exclusions this have been done
+# ranges       <- read.table( "~/Aerosols/source_R/PARAMETRIC/skip_date_ranges_CM21.txt",
+#                             sep = ";",
+#                             colClasses = "character",
+#                             header = TRUE, comment.char = "#" )
+# ranges$From  <- strptime(ranges$From,  format = "%F %H:%M", tz = "UTC")
+# ranges$Until <- strptime(ranges$Until, format = "%F %H:%M", tz = "UTC")
+#
+# for ( i in 1:nrow(ranges) ) {
+#     lower <- ranges$From[  i ]
+#     upper <- ranges$Until[ i ]
+#     ## select to remove
+#     select  <- gather$Date >= lower & gather$Date <= upper
+#     gather  <- gather[ ! select ]
+#     rm(select)
+# }
 
 
 
@@ -94,6 +100,7 @@ for ( i in 1:nrow(ranges) ) {
 ## monthly data
 temp <- gather[ , .( Mean   = mean(`[W.m-2]`,   na.rm = T),
                      Max    = max(`[W.m-2]`,    na.rm = T),
+                     Min    = min(`[W.m-2]`,    na.rm = T),
                      Median = median(`[W.m-2]`, na.rm = T)
 ), by = .(year(Date),month(Date)) ]
 
@@ -102,11 +109,18 @@ temp$Date <- as.POSIXct(strptime( paste( temp$year, temp$month, "1"), "%Y %m %d"
 
 plot(temp$Date, temp$Mean,   "l")
 plot(temp$Date, temp$Max,    "l")
+plot(temp$Date, temp$Min,    "l")
 plot(temp$Date, temp$Median, "l")
 
 
+gather$file    <- NULL
+gather$TIME_UT <- NULL
+names(gather)[names(gather) == "[W.m-2]"] <- "wattGLB"
 
-
+myRtools::writeDATA(gather,
+                    "~/DATA/cm21_data_validation/export_all_total.dat",
+                    contact = "natsisthanasis@gmail.com",
+                    type = "dat")
 
 tac = Sys.time()
 cat(sprintf("%s %s@%s %s %f mins\n\n",Sys.time(),Sys.info()["login"],Sys.info()["nodename"],Script.Name,difftime(tac,tic,units="mins")))
