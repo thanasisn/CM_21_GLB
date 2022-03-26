53da7dddb324921f5d8a28b280a06e07712eab96
thanasisn <11601717+thanasisn@users.noreply.github.com>
2022-03-26 11:32:04 +0200

    Auto commit

--- check_export_sirena.R
+++ check_export_sirena.R
@@ -1,11 +1,9 @@
 # /* #!/usr/bin/env Rscript */
 # /* Copyright (C) 2022 Athanasios Natsis <natsisthanasis@gmail.com> */
-#'
 #' ---
-#' title:         "Read global radiation data from sirena"
+#' title:         "Read and export global radiation data from sirena"
 #' author:        "Natsis Athanasios"
 #' date:          "`r format(Sys.time(), '%B %d, %Y')`"
-#' keywords:      "CM21, CM21 data validation, global irradiance"
 #' documentclass: article
 #' classoption:   a4paper,oneside
 #' fontsize:      11pt
@@ -17,19 +15,19 @@
 #' - \captionsetup{font=small}
 #'
 #' output:
-#'   html_document:
+#'   bookdown::html_document2:
+#'     keep_md:          yes
+#'     out_width:        100%
 #'     toc:              yes
-#'     keep_md:          no
-#'   bookdown::pdf_document2:
 #'     number_sections:  no
+#'   bookdown::pdf_document2:
 #'     fig_caption:      no
 #'     keep_tex:         no
-#'     keep_md:          no
 #'     latex_engine:     xelatex
+#'     number_sections:  no
 #'     toc:              yes
 #'   odt_document:  default
 #'   word_document: default
-#'
 #' ---
 
 #+ echo=F, include=T
@@ -41,18 +39,19 @@ knitr::opts_chunk$set(echo       = FALSE   )
 knitr::opts_chunk$set(include    = FALSE   )
 knitr::opts_chunk$set(include    = TRUE    )
 knitr::opts_chunk$set(comment    = ""      )
-knitr::opts_chunk$set(cache      = TRUE    )
+knitr::opts_chunk$set(cache      = TRUE   )
 
 
 # pdf output is huge too many point to plot
 # knitr::opts_chunk$set(dev        = "pdf"   )
 knitr::opts_chunk$set(dev        = "png"   )
-knitr::opts_chunk$set(fig.width  = 8       )
-knitr::opts_chunk$set(fig.height = 6       )
+# knitr::opts_chunk$set(fig.width  = 8       )
+# knitr::opts_chunk$set(fig.height = 6       )
+# knitr::opts_chunk$set(out.width  =  "50%"       )
+knitr::opts_chunk$set(fig.align  =  "center"       )
 
-knitr::opts_chunk$set(out.width  = "70%"    )
-knitr::opts_chunk$set(fig.align  = "center" )
 # knitr::opts_chunk$set(fig.pos    = '!h'     )
+
 
 #+ echo=F, include=F
 ####  Set environment  ####
@@ -70,7 +69,7 @@ Script.Base = sub("\\.R$","",Script.Name)
 
 
 library(data.table)
-
+library(pander)
 
 #### read all tot files from sirena #####
 rdsfile <- "~/DATA/cm21_data_validation/export_all_tot_dat.Rds"
@@ -108,26 +107,50 @@ DT[  st.dev   < -8,  st.dev   := NA ]
 
 
 #'
-#' ## Files read by year
+#' ## By year
+#'
+pander(
+    DT[ , .(Files      = length(unique(file)),
+            Datapoints = .N,
+            Missing = sum(is.na(`[W.m-2]`))), by = year(Date)]
+)
 #'
 #' Unique files: `r length(unique(DT$file))`
-#' Unique days:  {{length(unique(DT$Date))}}
-DT[ TIME_UT %/% 1 == 24, .N, by = year(Date)]
+#'
+#' Unique days:  `r length(unique(DT$Date))`
+#'
+
+missing <- DT[, .(Missing = sum(is.na(`[W.m-2]`))), by = as.Date(Date)]
+missing[ Missing == 0, Missing := NA]
+plot(missing$as.Date, missing$Missing,pch = 19, cex=0.5, main = "Missing data by day")
+
+missing <- DT[, .(Missing = sum(is.na(`[W.m-2]`))), by = .(year(Date), month(Date)) ]
+missing[, as.Date := as.Date(paste(year, month, "01"),"%Y %m %d")]
+plot(missing$as.Date, missing$Missing,pch = 19, cex=0.5, main = "Missing data by month")
+
 
 
 #'
 #' ## Data points by hour
 #'
 #+ echo=T, include=T
-table(DT$TIME_UT %/% 1)
-# hist( DT$TIME_UT %/% 1, breaks = 25)
+pander(
+    table(  DT$TIME_UT %/% 1 )
+)
 #'
+# hist( DT$TIME_UT %/% 1, breaks = 25)
+
 
 #' ## Data points by minute
-hist(round( ( DT$TIME_UT %% 1 ) * 60 ), breaks = 61)
+#'
+hist(round( ( DT$TIME_UT %% 1 ) * 60 ), breaks = -1:61)
 
 #+ echo=F, include=F
-DT[ TIME_UT %/% 1 == 24, unique(file)]
+# DT[ TIME_UT %/% 1 == 24, unique(file)]
+
+count <- DT[, .N, by = file]
+#' Do all files has 1440 records: `r all(count$N == 1440)`
+pander(table(count$N))
 
 
 ## add nice date
@@ -145,18 +168,18 @@ DT <- DT[ `[W.m-2]` > -5   , ]
 
 #+ echo=F
 #' ## Inspect all data
-summary(DT)
+pander(summary(DT))
 plot(DT$Date, DT$`[W.m-2]`)
 hist(DT$`[W.m-2]`)
 
 
-# ## apply exclusions this have been done for almost all data by me
-# ranges       <- read.table( "~/Aerosols/source_R/PARAMETRIC/skip_date_ranges_CM21.txt",
+# ## apply exclusions this have been done for almost all data  me
+# ranges       <- read.table( "~/Aerosols/source_R/PARAMETRICkip_date_ranges_CM21.txt",
 #                             sep = ";",
 #                             colClasses = "character",
-#                             header = TRUE, comment.char = "#" )
-# ranges$From  <- strptime(ranges$From,  format = "%F %H:%M", tz = "UTC")
-# ranges$Until <- strptime(ranges$Until, format = "%F %H:%M", tz = "UTC")
+#                             header = TRUE, comment.char = " )
+# ranges$From  <- strptime(ranges$From,  format = "%F %H:%M",  = "UTC")
+# ranges$Until <- strptime(ranges$Until, format = "%F %H:%M",  = "UTC")
 #
 # for ( i in 1:nrow(ranges) ) {
 #     lower <- ranges$From[  i ]
@@ -195,28 +218,28 @@ uplim   <- quantile(DT$wattGLB, na.rm = T, probs = perc)
 datespp <- DT[wattGLB > uplim, unique(as.Date(Date)) ]
 
 
-#' ## Too high values
+#' ## Extreme values
 #+ echo=F, include=T, results='asis'
-options(digits = 10)
+options(digits = 6)
 cat(paste("There are", length(datespp),
     "days with more than", uplim,
     "watts, representing", (1-perc) * 100,
     "% of the data.\n"))
 #'
-
-
+#'
+#' ## Plot all extreme days
+#'
 for (ad in datespp ) {
     pp <- DT[ as.Date(Date) == ad ]
-
     plot(  pp$Date, pp$wattGLB, "l", col = "green")
     points(pp$Date, pp$st.dev, col = "blue", pch=19, cex=.2)
     title(as.Date(ad, origin = "1970-01-01"))
 }
-
-
-
+#'
+#'
+#' ## Plot all extreme days in time series
+#'
 pp <- DT[ as.Date(Date) %in% datespp ]
-
 plot(pp$Date, pp$wattGLB, "l", col = "green")
 points(pp$Date, pp$st.dev, col = "blue", pch=19, cex=.2)
 
@@ -227,5 +250,7 @@ points(pp$Date, pp$st.dev, col = "blue", pch=19, cex=.2)
 #                     contact = "natsisthanasis@gmail.com",
 #                     type = "dat")
 
-tac = Sys.time()
+
+#' **END**
+tac <- Sys.time()
 cat(sprintf("%s %s@%s %s %f mins\n\n",Sys.time(),Sys.info()["login"],Sys.info()["nodename"],Script.Name,difftime(tac,tic,units="mins")))
