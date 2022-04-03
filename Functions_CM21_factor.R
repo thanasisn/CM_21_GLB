8c05e6fbe282ba7d0051a92005e86d39fc8ed305
thanasisn <11601717+thanasisn@users.noreply.github.com>
2022-04-03 12:07:15 +0300

    Auto commit

--- Functions_CM21_factor.R
+++ Functions_CM21_factor.R
@@ -1,28 +1,29 @@
 
 Sys.setenv(TZ = "UTC")
-## check date function
+
+## Check date help function
 is.POSIXct <- function(x) inherits(x, "POSIXct")
 
-## values from CM21_caldata_06.txt
-## define data
+## Calibration values from CM21_caldata_06.txt
 calibration_data <- matrix(
        c( "1991-01-01", 11.98E-6, 0.5E-2,
-          "1995-10-21", 11.98E-6, 2E-2,
-          "1995-11-01", 11.98E-6, 1E-2,
-          "2004-07-01", 11.98E-6, 4E-2,
-          "2005-12-05", 11.99E-6, 4E-2,
-          "2011-12-30", 11.96E-6, 4E-2,
-          "2012-01-31", 11.96E-6, 4E-2  ),
+          "1995-10-21", 11.98E-6,   2E-2,
+          "1995-11-01", 11.98E-6,   1E-2,
+          "2004-07-01", 11.98E-6,   4E-2,
+          "2005-12-05", 11.99E-6,   4E-2,
+          "2011-12-30", 11.96E-6,   4E-2,
+          "2012-01-31", 11.96E-6,   4E-2  ),
        byrow = TRUE,
        ncol = 3)
-## read to data frame
-calibration_data <- data.frame(Date = as.POSIXct( calibration_data[,1] ),
-                               Sens = as.numeric( calibration_data[,2] ),
-                               Gain = as.numeric( calibration_data[,3] ) )
 
-## interpolation functions
+## Format to data frame
+calibration_data <- data.frame(Date        = as.POSIXct( calibration_data[,1] ),
+                               Sensitivity = as.numeric( calibration_data[,2] ),
+                               Gain        = as.numeric( calibration_data[,3] ))
+
+## Interpolation functions with extend right rule
 sensitivity <- approxfun( x      = calibration_data$Date,
-                          y      = calibration_data$Sens,
+                          y      = calibration_data$Sensitivity,
                           rule   = 1:2  )
 
 gain        <- approxfun( x      = calibration_data$Date,
@@ -39,7 +40,6 @@ gain        <- approxfun( x      = calibration_data$Date,
 #' @param date POSIXct date for the factor
 #'
 #' @return (numeric) Conversion factor
-#' @family CM 21 functions
 #' @export
 cm21factor <- function(date) {
     if (is.POSIXct(date)) {
@@ -48,30 +48,4 @@ cm21factor <- function(date) {
         stop("input must be POSIXct.\n you gave : ",date)
     }
 }
-
-
-## current values used
-SENSITICM21  = 11.96    # μV/W/m^2
-CFACTOR      = 40000    # measurement instrument conversion CM21
-
-## create some test graphs
-dates <- seq(calibration_data$Date[1],Sys.time(),by="day")
-
-plot(dates, sensitivity(dates), pch = ".", main = "CM21 Sensitivity")
-points(calibration_data$Date, calibration_data$Sens, col = "green")
-
-plot(dates, gain(dates),        pch = ".", main = "CM21 Acquisition gain")
-points(calibration_data$Date, calibration_data$Gain,  col = "cyan")
-
-plot(dates, gain(dates) / sensitivity(dates),
-                                pch = ".", main = "CM21 signal to radiation factor")
-points(calibration_data$Date, calibration_data$Gain / calibration_data$Sens, col = "orange" )
-
-
-plot(calibration_data$Date, c(NA, diff(calibration_data$Sens)),
-     main = "CM21 calibration factor change")
-
-plot(calibration_data$Date, 100*c(NA, diff(calibration_data$Sens))/calibration_data$Sens ,
-     main = "CM21 calibration factor change %")
-
 
