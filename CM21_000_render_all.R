422ebdb79b718617729a9ad99defb839223f64e3
thanasisn <11601717+thanasisn@users.noreply.github.com>
2022-04-09 01:07:27 +0300

    Auto commit

--- CM21_000_render_all.R
+++ CM21_000_render_all.R
@@ -25,28 +25,28 @@ system("~/Aerosols/BASH_help/update_data_from_sirena.sh")
 
 
 
-# render("./CM21_R10_Read_raw_LAP.R",
-#        params = list( ALL_YEARS = TRUE ),
-#        clean                = T  ,
-#        output_dir           = "~/CM_21_GLB/REPORTS/REPORTS/")
-#
-#
-# render("./CM21_R20_Parse_Data.R",
-#        params = list( ALL_YEARS = TRUE ),
-#        clean                = T  ,
-#        output_dir           = "~/CM_21_GLB/REPORTS/REPORTS/")
-#
-#
-# render("./CM21_R30_Compute_dark.R",
-#        params = list(  ALL_YEARS = TRUE ),
-#        clean                = T  ,
-#        output_dir           = "~/CM_21_GLB/REPORTS/REPORTS/")
-#
-#
-# render("./CM21_R40_Missing_dark_construct.R",
-#        params = list(  ALL_YEARS = TRUE ),
-#        clean                = T  ,
-#        output_dir           = "~/CM_21_GLB/REPORTS/REPORTS/")
+render("./CM21_R10_Read_raw_LAP.R",
+       params = list( ALL_YEARS = TRUE ),
+       clean                = T  ,
+       output_dir           = "~/CM_21_GLB/REPORTS/REPORTS/")
+
+
+render("./CM21_R20_Parse_Data.R",
+       params = list( ALL_YEARS = TRUE ),
+       clean                = T  ,
+       output_dir           = "~/CM_21_GLB/REPORTS/REPORTS/")
+
+
+render("./CM21_R30_Compute_dark.R",
+       params = list(  ALL_YEARS = TRUE ),
+       clean                = T  ,
+       output_dir           = "~/CM_21_GLB/REPORTS/REPORTS/")
+
+
+render("./CM21_R40_Missing_dark_construct.R",
+       params = list(  ALL_YEARS = TRUE ),
+       clean                = T  ,
+       output_dir           = "~/CM_21_GLB/REPORTS/REPORTS/")
 
 
 render("./CM21_R50_Signal_to_GHI.R",
