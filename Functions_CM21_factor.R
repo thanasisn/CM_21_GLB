e26d101c0474f53257127fecca538cb21ee89c87
thanasisn <11601717+thanasisn@users.noreply.github.com>
2022-04-14 11:07:45 +0300

    Auto commit

--- Functions_CM21_factor.R
+++ Functions_CM21_factor.R
@@ -5,7 +5,7 @@ Sys.setenv(TZ = "UTC")
 is.POSIXct <- function(x) inherits(x, "POSIXct")
 
 ## Calibration values from CM21_caldata_06.txt
-calibration_data <- matrix(
+cm21_calibration_data <- matrix(
        c( "1991-01-01", 11.98E-6, 0.5E-2,
           "1995-10-21", 11.98E-6,   2E-2,
           "1995-11-02", 11.98E-6,   1E-2,  ## <- this is correct!!!
@@ -17,19 +17,19 @@ calibration_data <- matrix(
        ncol = 3)
 
 ## Format to data frame
-calibration_data <- data.frame(Date        = as.POSIXct( calibration_data[,1] ),
-                               Sensitivity = as.numeric( calibration_data[,2] ),
-                               Gain        = as.numeric( calibration_data[,3] ))
+cm21_calibration_data <- data.frame(Date        = as.POSIXct( cm21_calibration_data[,1] ),
+                                    Sensitivity = as.numeric( cm21_calibration_data[,2] ),
+                                    Gain        = as.numeric( cm21_calibration_data[,3] ))
 
 ## Interpolation functions with extend right rule
-sensitivity <- approxfun( x      = calibration_data$Date,
-                          y      = calibration_data$Sensitivity,
-                          rule   = 1:2  )
+cm21_sensitivity <- approxfun( x      = cm21_calibration_data$Date,
+                               y      = cm21_calibration_data$Sensitivity,
+                               rule   = 1:2  )
 
-gain        <- approxfun( x      = calibration_data$Date,
-                          y      = calibration_data$Gain,
-                          method = "constant",
-                          rule   = 1:2  )
+cm21_gain        <- approxfun( x      = cm21_calibration_data$Date,
+                               y      = cm21_calibration_data$Gain,
+                               method = "constant",
+                               rule   = 1:2  )
 
 
 #' Conversion factor from volt to watt for CM21
@@ -43,7 +43,7 @@ gain        <- approxfun( x      = calibration_data$Date,
 #' @export
 cm21factor <- function(date) {
     if (is.POSIXct(date)) {
-        return( gain(date) / sensitivity(date) )
+        return( cm21_gain(date) / cm21_sensitivity(date) )
     } else {
         stop("input must be POSIXct.\n you gave : ",date)
     }
