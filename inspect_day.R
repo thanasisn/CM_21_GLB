66694fbae89f7eb812a9b723b9b77a653fc4c656
thanasisn <11601717+thanasisn@users.noreply.github.com>
2022-04-14 11:07:45 +0300

    Auto commit

--- inspect_day.R
+++ inspect_day.R
@@ -8,6 +8,8 @@ tic <- Sys.time()
 
 library(data.table, quietly = T, warn.conflicts = F)
 library(optparse)
+source("~/CM_21_GLB/Functions_CM21_factor.R")
+source("~/CHP_1_DIR/Functions_CHP1.R")
 
 
 ## Data folder
@@ -75,18 +77,57 @@ if (STEP == 1) {
 
 daystodo <- seq(STARTDAY, MAXDATE, by = MOVE)
 
+## loop plots
 for (ap in daystodo) {
     toplot <- as.Date(ap:(ap+STEP), origin = "1970-01-01")
     gather <- data.table()
 
+    ## loop days to plot with step
     for (ad in toplot) {
-        theday <- as.Date(ad, origin = "1970-01-01")
-        format(theday,"%d%m%y")
+        theday    <- as.Date(ad, origin = "1970-01-01")
+        strday    <- format(theday,"%d%m%y")
 
+        glbfile   <- grep( strday, globalfiles, value = T )[1]
+        dirfile   <- grep( strday, directfiles, value = T )[1]
 
+        D_minutes <- seq(from       = as.POSIXct(paste(theday,"00:00:30 UTC")),
+                         length.out = 1440,
+                         by         = "min" )
 
+        if (file.exists(glbfile)) {
+            glb <- fread(glbfile)
+            names(glb) <- c("GLBsig", "GLBsd")
+            glb[ GLBsig < -8, GLBsig := NA ]
+            glb[ GLBsd  < -8, GLBsd  := NA ]
+        } else {
+            cat(paste0("Missing global : ", strday,"06"),"\n")
+            glb        <- data.table()
+            glb$GLBsig <- rep(NA, 1440)
+            glb$GLBsd  <- rep(NA, 1440)
+        }
+        if (file.exists(dirfile)){
+            dir <- fread(dirfile)
+            names(dir) <- c("DIRsig", "DIRsd")
+            dir[ DIRsig < -8, DIRsig := NA ]
+            dir[ DIRsd  < -8, DIRsd  := NA ]
+        } else {
+            cat(paste0("Missing direct : ", strday,"03"),"\n")
+            dir        <- data.table()
+            dir$DIRsig <- rep(NA, 1440)
+            dir$DIRsd  <- rep(NA, 1440)
+        }
 
+        daydt  <- cbind(Date = D_minutes,glb, dir)
+        gather <- rbind(gather, daydt)
     }
+
+    ## convert to radiation
+    gather$GLBsig <- gather$GLBsig * cm21factor(gather$Date)
+    gather$GLBsd  <- gather$GLBsd  * cm21factor(gather$Date)
+    gather$DIRsig <- gather$DIRsig * chp1factor(gather$Date)
+    gather$DIRsd  <- gather$DIRsd  * chp1factor(gather$Date)
+
+    stop()
 
 }
 
