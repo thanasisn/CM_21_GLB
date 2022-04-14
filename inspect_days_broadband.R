849b01ff3c3a6e423ea057dcb87a62db832aee2f
thanasisn <11601717+thanasisn@users.noreply.github.com>
2022-04-14 17:07:17 +0300

    Auto commit

---
+++ inspect_days_broadband.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+# /* Copyright (C) 2022 Athanasios Natsis <natsisthanasis@gmail.com> */
+
+rm(list = (ls()[ls() != ""]))
+Sys.setenv(TZ = "UTC")
+tic <- Sys.time()
+
+
+## TODO 
+## similar to inspect_days_Lap.R
+## read from rds files
+## select to plot SNG, S0, S1, L1, L2
+
+
+library(data.table, quietly = T, warn.conflicts = F)
+library(optparse,   quietly = T, warn.conflicts = F)
+library(plotly,     quietly = T, warn.conflicts = F)
+
+
