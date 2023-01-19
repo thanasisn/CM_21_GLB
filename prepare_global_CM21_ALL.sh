c7516e04c513631e1b2cec4dd98e411d2f48ef4d
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-01-19 18:43:39 +0200

    Commit 2023-01-19 18:43

--- prepare_global_CM21_ALL.sh
+++ prepare_global_CM21_ALL.sh
@@ -4,7 +4,6 @@
 #### Run daily the data process.
 ## For building reports there is an R scirpt
 
-
 info() { echo ; echo "$(date +%F_%T) :: $* " >&1; }
 mkdir -p "$(dirname "$0")/LOGs/"
 LOG_FILE="$(dirname "$0")/LOGs/$(basename "$0")_$(date +%F_%T).log"
@@ -19,22 +18,22 @@ info "Try to get new files"
 "$(dirname "$0")/get_data_from_sirena.sh"
 
 info "Read raw files to SIG"
-Rscript "$(dirname "$0")/CM21_R10_Read_raw_LAP.R" "ALL"
+Rscript "$(dirname "$0")/CM21_R10_Read_raw_LAP.R"           "ALL"
 
 info "Filter raw data to S0"
-Rscript "$(dirname "$0")/CM21_R20_Parse_Data.R" "ALL"
+Rscript "$(dirname "$0")/CM21_R20_Parse_Data.R"             "ALL"
 
 info "Compute dark to S1"
-Rscript "$(dirname "$0")/CM21_R30_Compute_dark.R" "ALL"
+Rscript "$(dirname "$0")/CM21_R30_Compute_dark.R"           "ALL"
 
 info "Construct missing dark for S1"
 Rscript "$(dirname "$0")/CM21_R40_Missing_dark_construct.R" "ALL"
 
 info "Convert S1 to L0 GHI"
-Rscript "$(dirname "$0")/CM21_R50_Signal_to_GHI.R" "ALL"
+Rscript "$(dirname "$0")/CM21_R50_Signal_to_GHI.R"          "ALL"
 
 info "Clean output L0 to L1 GHI"
-Rscript "$(dirname "$0")/CM21_R60_GHI_output.R" "ALL"
+Rscript "$(dirname "$0")/CM21_R60_GHI_output.R"             "ALL"
 
 
 ## extra output
@@ -52,7 +51,7 @@ bwlim=500
 rclone="$HOME/PROGRAMS/rclone"
 config="$HOME/Documents/rclone.conf"
 otheropt=" --checkers=20 --delete-before --stats=300s"
-bwlimit="--bwlimit=${bwlim}k"
+bwlimit="  --bwlimit=${bwlim}k"
 
 
 
@@ -61,8 +60,6 @@ info "Upload  CM21_Reports"
 
 info "Upload  CM21_Daily"
 "${rclone}" ${otheropt} "${bwlimit}" --config "$config" copy "$HOME/CM_21_GLB/REPORTS/DAILY"   lapauththanasis:/Aerosols/CM21_Daily
-
-
 
 
 ## end coding
