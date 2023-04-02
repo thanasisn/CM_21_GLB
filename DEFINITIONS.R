47877e577e06e70816b70ec94f994e43f7b4269f
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-04-02 08:13:16 +0300

    Commit 2023-04-02 08:13

--- DEFINITIONS.R
+++ DEFINITIONS.R
@@ -84,7 +84,7 @@ GLB_LOW_LIM_02 <- -7       ## after break-data
 
 
 ####   Dark Calculations and definitions    ####################################
-DSTRETCH    <-  20 * 3600  ## Extend of dark signal for morning and evening of the same day (R30)
+DSTRETCH    <-   3 * 3600  ## Extend of dark signal for morning and evening of the same day (R30)
 DCOUNTLIM   <-  10         ## Number of valid measurements to compute dark (R30)
 DARK_ELEV   <- -10         ## Sun elevation limit to get dark signal (R20, R30)
 MINLIMnight <- -15         ## Lower radiation limit  when dark       (R20) -> ToolowDark
