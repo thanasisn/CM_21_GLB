4c51201cc289190a74f1b2af0ea7140d6c86f536
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-10-09 23:09:29 +0300

    Commit 2024-10-09 23:09

--- prepare_global_CM21_ALL.sh
+++ prepare_global_CM21_ALL.sh
@@ -13,57 +13,7 @@ exec  > >(tee -i "${LOG_FILE}")
 exec 2> >(tee -i "${ERR_FILE}" >&2)
 info "START :: $0 :: $* ::"
 
-
-
-## info "Try to get new files"
-## "$(dirname "$0")/get_data_from_sirena.sh"
-
-## Deprecated: info "Read raw files to SIG"
-## Deprecated: Rscript "$(dirname "$0")/CM21_R10_Read_raw_LAP.R"           "ALL"
-
-## Deprecated: info "Filter raw data to S0"
-## Deprecated: Rscript "$(dirname "$0")/CM21_R20_Parse_Data.R"             "ALL"
-
-## /opt/R/4.2.3/bin/Rscript "$HOME/BBand_LAP/process/Legacy_CM21_R20_export.R" 
-## /opt/R/4.2.3/bin/Rscript "$HOME/BBand_LAP/process/Legacy_CM21_R30_export.R" 
-## /opt/R/4.2.3/bin/Rscript "$HOME/BBand_LAP/process/Legacy_CM21_R50_export.R" 
 /opt/R/4.2.3/bin/Rscript "$HOME/BBand_LAP/process/Legacy_CM21_R60_export.R" 
-
-## Deprecated: info "Compute dark to S1"
-## Deprecated: Rscript "$(dirname "$0")/CM21_R30_Compute_dark.R"           "ALL"
-
-## Deprecated: info "Construct missing dark for S1"
-## Deprecated: Rscript "$(dirname "$0")/CM21_R40_Missing_dark_construct.R" "ALL"
-
-## Deprecated: info "Convert S1 to L0 GHI"
-## Deprecated: Rscript "$(dirname "$0")/CM21_R50_Signal_to_GHI.R"          "ALL"
-
-## info "Clean output L0 to L1 GHI"
-## Rscript "$(dirname "$0")/CM21_R60_GHI_output.R"                         "ALL"
-
-
-## extra output to run occational and inspect output
-# info "Export for WRDC"
-# Rscript "$(dirname "$0")/CM21_T70_GHI_Export_WRDC.R"
-
-# info "Export for Sirena"
-# Rscript "$(dirname "$0")/CM21_T71_GHI_Export_TOT.R"
-
-
-
-## rclone options
-bwlim=500
-rclone="$HOME/PROGRAMS/rclone"
-config="$HOME/Documents/rclone.conf"
-otheropt=" --checkers=20 --delete-before --stats=300s"
-bwlimit=" --bwlimit=${bwlim}k"
-
-
-info "Upload  CM21_Reports"
-"${rclone}" ${otheropt} ${bwlimit} --config "$config" copy "$HOME/CM_21_GLB/REPORTS/REPORTS" lapauththanasis:/Aerosols/CM21_Reports
-
-info "Upload  CM21_Daily"
-"${rclone}" ${otheropt} ${bwlimit} --config "$config" copy "$HOME/CM_21_GLB/REPORTS/DAILY"   lapauththanasis:/Aerosols/CM21_Daily
 
 
 ## end coding
